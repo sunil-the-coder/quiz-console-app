0b32b19e7ee8fa75467bd5aca2d9b80994b009a9
Sunil Patil <sunilpatil10june@gmail.com>
2025-02-01 13:58:02 +0530

    updated quiz app client

--- src/com/devgen/quiz/client/QuizAppClient.java
+++ src/com/devgen/quiz/client/QuizAppClient.java
@@ -7,7 +7,7 @@ public class QuizAppClient {
     
     public static void main(String[] args) {
         
-    	QuestionService questionService = new QuestionService();
+    	QuestionService questionService = new QuestionService(10);
     	QuizService quizService = new QuizService(questionService);
     	quizService.playQuiz();
     	quizService.printFinalScore();
